48c0a39f56670dfc1f309063e708b1a51eda78f8
Henrique L. Silva <henriquelsilva911@gmail.com>
2025-05-20 16:29:18 -0300

    Added CMake

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/henrique/Downloads/unitConverter/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  "/home/henrique/Downloads/unitConverter/src/DistanceConverter.cpp" "CMakeFiles/main.dir/src/DistanceConverter.cpp.o" "gcc" "CMakeFiles/main.dir/src/DistanceConverter.cpp.o.d"
+  "/home/henrique/Downloads/unitConverter/src/TemperatureConverter.cpp" "CMakeFiles/main.dir/src/TemperatureConverter.cpp.o" "gcc" "CMakeFiles/main.dir/src/TemperatureConverter.cpp.o.d"
+  "/home/henrique/Downloads/unitConverter/src/WeightConverter.cpp" "CMakeFiles/main.dir/src/WeightConverter.cpp.o" "gcc" "CMakeFiles/main.dir/src/WeightConverter.cpp.o.d"
+  "" "main" "gcc" "CMakeFiles/main.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
